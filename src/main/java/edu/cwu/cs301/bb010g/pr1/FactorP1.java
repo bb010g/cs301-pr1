ad922297186d49cc5ddf5c96dd1635907aad76b4
Brayden Banks <bb010g@gmail.com>
2016-10-10 11:30:21 -0700

    Our first real dependency!
    It's Guava because Java's BigInt doesn't seem to know how to sqrt.
    Wheee, I'll probably end up just having build give a dist jar now. or I
    could see how to extract `gradle run`'s classpath. Probably the latter
    because slightly more simplicity.

--- src/main/java/edu/cwu/cs301/bb010g/pr1/FactorP1.java
+++ src/main/java/edu/cwu/cs301/bb010g/pr1/FactorP1.java
@@ -1,5 +1,78 @@
 package edu.cwu.cs301.bb010g.pr1;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.math.BigIntegerMath;
+
 public class FactorP1 {
-  public static void main(final String[] args) {}
+  public static void main(final String[] args) {
+    try (final FileInputStream fis = new FileInputStream("integer.txt");
+        final InputStreamReader isr = new InputStreamReader(fis);
+        final BufferedReader br = new BufferedReader(isr);) {
+
+      final BigInteger n = new BigInteger(br.readLine());
+      final Pair<List<BigInteger>, Integer> factoring = FactorP1.factor(n);
+      System.err.println("sqrt calls: " + factoring.snd);
+      System.out.println(factoring.fst);
+
+    } catch (FileNotFoundException e) {
+      System.out.println("File integer.txt is missing.");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  static final BigInteger ZERO = BigInteger.ZERO;
+  static final BigInteger ONE = BigInteger.ONE;
+  static final BigInteger TWO = BigInteger.valueOf(2);
+
+  public static Pair<List<BigInteger>, Integer> factor(BigInteger n) {
+    final List<BigInteger> factors = new ArrayList<>();
+    int sqrtCount = 0;
+    factoring: while (true) {
+      if (n.remainder(TWO).equals(ZERO)) {
+        factors.add(TWO);
+        if (n.equals(TWO)) {
+          break factoring;
+        }
+        n = n.divide(TWO);
+        continue factoring;
+      }
+      sqrtCount++;
+      BigInteger x = BigIntegerMath.sqrt(n, RoundingMode.FLOOR);
+      if (x.pow(2).equals(n)) {
+        factors.add(x);
+        n = x;
+        continue factoring;
+      }
+      while (true) {
+        x = x.add(ONE);
+        if (x.equals(n.add(ONE).divide(TWO))) {
+          factors.add(n);
+          break factoring;
+        }
+        sqrtCount++;
+        final BigInteger yInner = x.pow(2).subtract(n);
+        final BigInteger y = BigIntegerMath.sqrt(yInner, RoundingMode.FLOOR);
+        if (y.pow(2).equals(yInner)) {
+          BigInteger z = x.add(y);
+          if (!n.remainder(z).equals(ZERO)) {
+            z = x.subtract(y);
+          }
+          factors.add(z);
+          n = n.divide(z);
+          continue factoring;
+        }
+      }
+    }
+    return Pair.of(factors, sqrtCount);
+  }
 }
---
+++ src/main/java/edu/cwu/cs301/bb010g/pr1/Pair.java
@@ -0,0 +1,58 @@
+package edu.cwu.cs301.bb010g.pr1;
+
+public class Pair<A, B> {
+  public A fst;
+  public B snd;
+
+  public Pair(final A fst, final B snd) {
+    this.fst = fst;
+    this.snd = snd;
+  }
+  
+  public static <A, B> Pair<A, B> of(final A fst, final B snd) {
+    return new Pair<>(fst, snd);
+  }
+
+  @Override
+  public String toString() {
+    return "Pair[" + this.fst + ", " + this.snd + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((this.fst == null) ? 0 : this.fst.hashCode());
+    result = prime * result + ((this.snd == null) ? 0 : this.snd.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (this.getClass() != obj.getClass()) {
+      return false;
+    }
+    final Pair<?, ?> other = (Pair<?, ?>) obj;
+    if (this.fst == null) {
+      if (other.fst != null) {
+        return false;
+      }
+    } else if (!this.fst.equals(other.fst)) {
+      return false;
+    }
+    if (this.snd == null) {
+      if (other.snd != null) {
+        return false;
+      }
+    } else if (!this.snd.equals(other.snd)) {
+      return false;
+    }
+    return true;
+  }
+}
